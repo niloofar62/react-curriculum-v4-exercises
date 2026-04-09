49f2d2b87334cb9b329f4fffb9bd594be8e516b2
Niloofar Nayebi <nil_nyb@yahoo.com>
2026-04-08 23:00:21 -0400

    add About Me screenshot

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -2,11 +2,27 @@
 //Exercise: Build an "About Me" Component in this file
 
 export default function StudentWork() {
-  //add variables here
+  // add variables here
+  const name = 'Niloofar';
+  const age = 34;
+  const hobbies = ['Design', 'Coding', 'Traveling'];
+
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me</h1>
+
+      <p>
+        Hi, my name is {name}. I am {age} years old and I love building apps and
+        learning new things.
+      </p>
+
+      <h2>My Hobbies:</h2>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
